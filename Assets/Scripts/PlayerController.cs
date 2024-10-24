42e7e7e96d2a6a1f3a151bfae19984640f521dc1
Ceoofe <146845430+Ceoofe@users.noreply.github.com>
2024-10-24 12:36:12 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private int speed = 8;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "GoodTag")
+        {
+            Destroy(collision.gameObject);
+            score = score + 1;
+            Debug.Log(score);
+        }
+        else if(collision.gameObject.tag == "BadTag")
+        {
+            Destroy(collision.gameObject);
+            score = 0;
+            Debug.Log("You lost all of your points!");
+            Debug.Log(score);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnItems.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour
+{
+    public GameObject[] itemsPrefabs;
+    private float spawnRangeX = 7f;
+    private float spawnRangeZ = 7f;
+    private float delay = 2f;
+    private float spawninterval = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Clone", delay, spawninterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void Clone()
+    {
+        int itemsIndex = Random.Range(0, itemsPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 10, Random.Range(-spawnRangeZ, spawnRangeZ));
+        Instantiate(itemsPrefabs[itemsIndex], spawnPos, itemsPrefabs[itemsIndex].transform.rotation);
+    }
+}
